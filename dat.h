a12936c5120110c9dc786581cac43e86efe21ba9
Konstantinn Bonnet <qu7uux@gmail.com>
2016-02-21 17:33:32 +0100

    audio: partial removal of version defs bullshit
    - one binary for all, assuming wl6 by default, change via arg(2)
    - this currently further breaks everything since name collisions are not
      handled; file loading will be done first

--- dat.h
+++ dat.h
@@ -1,5 +1,12 @@
 typedef short s16int;
 typedef int s32int;
 
+extern int sod;
+extern int sdm;
+extern int wl1;
+extern char *ext;
+
 extern u32int pal[];
 extern u8int intro[];
+
+#include "audio.h"
